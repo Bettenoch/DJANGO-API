2ecb55d1b60ec9a370751e96c527ab4f87da3c59
Bettenoch <bettenoch254@gmail.com>
2024-03-08 09:45:18 +0300

    Create login and refresh url routers.

--- core/routers.py
+++ core/routers.py
@@ -1,10 +1,15 @@
 from rest_framework import routers
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets.refresh import RefreshViewet
+from core.auth.viewsets.register import RegisterViewSet, LoginViewSet
 from core.user.viewsets import UserViewSet
+
+
 router = routers.SimpleRouter()
 
 router.register(r'users', UserViewSet, basename='user')
-router.register(r'auth/register', RegisterViewSet, basename='auth/register')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
+routers.register(r'auth/refresh', RefreshViewet, basename='auth-refresh')
 urlpatterns = [
     *router.urls
 ]
