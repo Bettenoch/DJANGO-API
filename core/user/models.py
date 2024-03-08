30ba1fc8eda3acf0877b7a3f11bfb880b0f88181
Bettenoch <bettenoch254@gmail.com>
2024-03-08 12:02:25 +0300

    Replace methods with abstract class

--- core/user/models.py
+++ core/user/models.py
@@ -5,14 +5,10 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.http import Http404
+from core.abstract.models import AbstractManager, AbstractModel
 
-class UserManager(BaseUserManager):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
+class UserManager(BaseUserManager, AbstractManager):
+
     def create_user(self, username, email, password=None, **kwargs):
         if username is None:
             raise TypeError('User must have a username')
@@ -39,39 +35,8 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
-    
-    def create_user(self, username, email, password=None, **kwargs):
-        if username is None:
-            raise TypeError('User must have a username')
-        if email is None:
-            raise TypeError('User must have an email')
-        if password is None:
-            raise TypeError('User must have a password')
-        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    def create_superuser(self, username, email, password, **kwargs):
-        if password is None:
-            raise TypeError('Superuser must have a password');
-        if email is None:
-            raise TypeError('Superuser must have an email');
-        if username is None:
-            raise TypeError('Superuser must have a username');
-        user = self.create_user(username, email, password, **kwargs)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+class User(AbstractBaseUser, PermissionsMixin, AbstractModel):
+   
     username = models.CharField(max_length=255, db_index=True, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -80,8 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, db_index=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects  = UserManager()
@@ -92,5 +56,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def name(self):
         return f"{self.first_name} {self.last_name}"
-
-# data_user = {"email":"testemail@gmail.com", "username":"bett-enock", "password":"bett254", "first_name:"bett", "last_name":"enock"}
