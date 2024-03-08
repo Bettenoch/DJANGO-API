185e2c09b949c3ed4b57363135e1d8f8f7edf8e1
Bettenoch <bettenoch254@gmail.com>
2024-03-08 12:04:51 +0300

    Use abtract serailzer in user serializer class.

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from core.user.models import User
+from core.abstract.serializers import AbstractSerializer
 
-class UserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(source='public_id', readonly=True, format='hex')
-    created = serializers.DateTimeField(readonly=True)
-    updated = serializers.DateTimeField(readonly=True)
-    
+class UserSerializer(AbstractSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'bio', 'avatar', 'is_active', 'created', 'updated']
