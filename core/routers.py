565791b63273adf7dead840a3e8094f287157049
Bettenoch <bettenoch254@gmail.com>
2024-03-08 22:37:32 +0300

    create post viewset router url.

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from core.auth.viewsets.refresh import RefreshViewet
 from core.auth.viewsets.register import RegisterViewSet, LoginViewSet
+from core.post.viewsets import PostViewSet
 from core.user.viewsets import UserViewSet
 
 
@@ -9,7 +10,8 @@ router = routers.SimpleRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
-routers.register(r'auth/refresh', RefreshViewet, basename='auth-refresh')
+router.register(r'auth/refresh', RefreshViewet, basename='auth-refresh')
+router.register(r'post', PostViewSet, basename='post')
 urlpatterns = [
     *router.urls
 ]
